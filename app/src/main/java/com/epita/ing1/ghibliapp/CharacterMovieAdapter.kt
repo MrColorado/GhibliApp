9bf20ac3d831d12fbd33a5bdc73ef997fd508e79
MrColorado <theo.issarni@epita.fr>
2018-06-07 01:00:02 +0200

    character: display it's movies but it's only fucking url

---
+++ app/src/main/java/com/epita/ing1/ghibliapp/CharacterMovieAdapter.kt
@@ -0,0 +1,34 @@
+package com.epita.ing1.ghibliapp
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class CharacterMovieAdapter(val context: Context,
+                            val data: List<String>) :
+        RecyclerView.Adapter<CharacterMovieAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameTextView: TextView = itemView.findViewById(R.id.text_view_character_movie)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val rowView = LayoutInflater
+                .from(context)
+                .inflate(R.layout.character_movie_list, parent, false)
+        val viewHolder = ViewHolder(rowView)
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = data[position]
+        holder.nameTextView.text = currentItem
+    }
+}
