5a858bc1d63d9dc5e68994f8cadb29d23ba5027a
sidore_m <marc.sidorenko@epita.fr>
2018-06-06 14:37:48 +0200

    Fix QuizzActivity

--- app/src/main/java/com/epita/ing1/ghibliapp/QuizzActivity.kt
+++ app/src/main/java/com/epita/ing1/ghibliapp/QuizzActivity.kt
@@ -1,4 +1,12 @@
 package com.epita.ing1.ghibliapp
 
-class QuizzActivity {
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizzActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quizz)
+    }
 }
