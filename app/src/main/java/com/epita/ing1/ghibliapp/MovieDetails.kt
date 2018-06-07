a9e3b29e79359661282efac7fddec4ede5159676
MrColorado <theo.issarni@epita.fr>
2018-06-07 20:42:03 +0200

    charactermovies: movies are working

--- app/src/main/java/com/epita/ing1/ghibliapp/MovieDetails.kt
+++ app/src/main/java/com/epita/ing1/ghibliapp/MovieDetails.kt
@@ -1,3 +1,6 @@
 package com.epita.ing1.ghibliapp
 
-class MovieDetails(val title: String, val director: String, val year: Int, val description: String)
+data class MovieDetails(
+            val id: String,
+            val title: String
+            )
