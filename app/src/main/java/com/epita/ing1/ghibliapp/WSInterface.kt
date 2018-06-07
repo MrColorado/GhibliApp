b4569334878a4b6c78317f646af8c9b183a094ed
Rod Guillaume <rodguillaume@tuta.io>
2018-06-07 17:52:57 +0200

    MOVIE add WebService interface for retrofit requests

---
+++ app/src/main/java/com/epita/ing1/ghibliapp/WSInterface.kt
@@ -0,0 +1,13 @@
+package com.epita.ing1.ghibliapp
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WSInterface {
+    @GET("films")
+    fun getFilms() : Call<List<Movie>>
+
+    @GET("films")
+    fun getFilmDetails(@Query("id") movidId : Int) : Call<MovieDetails>
+}
