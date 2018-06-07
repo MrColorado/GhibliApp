a1bc1f383fbdd32bc4cbd87f03cf2301c8b9ea82
sidore_m <marc.sidorenko@epita.fr>
2018-06-07 15:41:13 +0200

    Update Quizz answers handling

--- app/src/main/java/com/epita/ing1/ghibliapp/QuizzActivity.kt
+++ app/src/main/java/com/epita/ing1/ghibliapp/QuizzActivity.kt
@@ -2,6 +2,9 @@ package com.epita.ing1.ghibliapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -25,33 +28,75 @@ data class PeopleQuizz(
         val species: String,
         val url: String)
 
+data class QuizzItem(
+        val id: String,
+        val title: String,
+        val question: String,
+        val correct: Int,
+        val answer1: String,
+        val answer2: String,
+        val answer3: String
+)
+
 class QuizzActivity : AppCompatActivity() {
 
-    val quests: MutableList<String> = mutableListOf()
+    var tabAn: MutableList<Button> = mutableListOf()
+
+    var tabLab: MutableList<TextView> = mutableListOf()
+
+    var score: Int = 0
+
+    var count: Int = 1
+
+    var max: Int = 10
+
+    var quests: MutableList<QuizzItem> = mutableListOf()
+
+    var currentQuest: QuizzItem? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quizz)
+
+        currentQuest = QuizzItem("Test", "QUIZZ test", "How's the daddy ?",
+                2, "Jojo", "Me", "Epita lambda student.")
+        val txt1: TextView = findViewById(R.id.TitleText)
+        val txt2: TextView = findViewById(R.id.QuestionText)
+        val txt3: TextView = findViewById(R.id.ScoreText)
+        val an1: Button = findViewById(R.id.ButtonAn1)
+        val an2: Button = findViewById(R.id.ButtonAn2)
+        val an3: Button = findViewById(R.id.ButtonAn3)
+        val next: Button = findViewById(R.id.NextQuestButton)
+        an1.setVisibility(View.VISIBLE)
+        an2.setVisibility(View.VISIBLE)
+        an3.setVisibility(View.VISIBLE)
+        tabAn.add(an1)
+        tabAn.add(an2)
+        tabAn.add(an3)
+        tabAn.add(next)
+        tabLab.add(txt1)
+        tabLab.add(txt2)
+        tabLab.add(txt3)
 
         val dataPeople = arrayListOf<PeopleQuizz>()
 
         val jsonConv = GsonConverterFactory.create(GsonBuilder().create())
 
         val retrofit = Retrofit.Builder()
-                .baseUrl("https://ghibliapi.herokuapp.com/")
+                .baseUrl("https://ghibliapi.herokuapp.com/people/")
                 .addConverterFactory(jsonConv)
                 .build()
 
-        val service = retrofit.create(PeopleQuizz::class.java)
+        // val service = retrofit.create(PeopleQuizz::class.java)
 
-        val callback = object  : Callback<List<PeopleQuizz>> {
+        val callback = object : Callback<List<PeopleQuizz>> {
             override fun onFailure(call: Call<List<PeopleQuizz>>?, t: Throwable?) {
-// Code here what happens if calling the WebService fails
+                // Code here what happens if calling the WebService fails
                 //button.text = "FAIL1"
             }
 
             override fun onResponse(call: Call<List<PeopleQuizz>>?, response: Response<List<PeopleQuizz>>?) {
-// Code here what happens when WebService responds
+                // Code here what happens when WebService responds
                 // button.text = "Fail2"
                 if (response != null) {
                     // button.text = "Fail3"
@@ -65,5 +110,104 @@ class QuizzActivity : AppCompatActivity() {
                 }
             }
         }
+
+        // start Game
+        StartQuestions()
+    }
+
+    fun StartQuestions() {
+        // Change Question
+        // Update Question Labels
+        this.tabLab[0].text = currentQuest!!.title
+        this.tabLab[1].text = currentQuest!!.question
+        // Update Buttons values
+        this.tabAn[0].text = currentQuest!!.answer1
+        this.tabAn[1].text = currentQuest!!.answer2
+        this.tabAn[2].text = currentQuest!!.answer3
+        // Buttons Show
+        this.tabAn[0].visibility = View.VISIBLE
+        this.tabAn[1].visibility = View.VISIBLE
+        this.tabAn[2].visibility = View.VISIBLE
+        this.tabAn[3].visibility = View.INVISIBLE
+    }
+
+    fun nextQuestion(clickedView: View) {
+        // Increm
+        count++
+        // Change Question
+        if (count > max) {
+            this.tabLab[0].text = "Finish !"
+            this.tabLab[1].text = ""
+            this.tabLab[2].text = "Score :" + score + "/" + max
+            this.tabAn[0].visibility = View.INVISIBLE
+            this.tabAn[1].visibility = View.INVISIBLE
+            this.tabAn[2].visibility = View.INVISIBLE
+            this.tabAn[3].visibility = View.INVISIBLE
+        } else {
+            // Update Question Labels
+            this.tabLab[0].text = currentQuest!!.title
+            this.tabLab[1].text = currentQuest!!.question
+            // Update Buttons values
+            this.tabAn[0].text = currentQuest!!.answer1
+            this.tabAn[1].text = currentQuest!!.answer2
+            this.tabAn[2].text = currentQuest!!.answer3
+            // Buttons Show
+            this.tabAn[0].visibility = View.VISIBLE
+            this.tabAn[1].visibility = View.VISIBLE
+            this.tabAn[2].visibility = View.VISIBLE
+            this.tabAn[3].visibility = View.INVISIBLE
+        }
+    }
+
+    fun onClickAn1(clickedView: View) {
+        this.tabLab[1].text = "Answer is :"
+        if (this.currentQuest!!.correct == 1) {
+            this.score += 1
+            this.tabAn[1].visibility = View.INVISIBLE
+            this.tabAn[2].visibility = View.INVISIBLE
+        } else if (this.currentQuest!!.correct == 2) {
+            this.tabAn[2].visibility = View.INVISIBLE
+            this.tabAn[0].visibility = View.INVISIBLE
+        } else {
+            this.tabAn[0].visibility = View.INVISIBLE
+            this.tabAn[1].visibility = View.INVISIBLE
+        }
+        this.tabAn[3].visibility = View.VISIBLE
+    }
+
+    fun onClickAn2(clickedView: View) {
+        this.tabLab[1].text = "Answer is :"
+        if (this.currentQuest!!.correct == 2) {
+            this.score += 1
+            this.tabAn[0].visibility = View.INVISIBLE
+            this.tabAn[2].visibility = View.INVISIBLE
+        } else if (this.currentQuest!!.correct == 1) {
+            this.tabAn[1].visibility = View.INVISIBLE
+            this.tabAn[2].visibility = View.INVISIBLE
+        } else {
+            this.tabAn[1].visibility = View.INVISIBLE
+            this.tabAn[0].visibility = View.INVISIBLE
+        }
+        this.tabAn[3].visibility = View.VISIBLE
+    }
+
+    fun onClickAn3(clickedView: View) {
+        this.tabLab[1].text = "Answer is :"
+        if (this.currentQuest!!.correct == 3) {
+            this.score += 1
+            this.tabAn[0].visibility = View.INVISIBLE
+            this.tabAn[1].visibility = View.INVISIBLE
+        } else if (this.currentQuest!!.correct == 1) {
+            this.tabAn[1].visibility = View.INVISIBLE
+            this.tabAn[2].visibility = View.INVISIBLE
+        } else {
+            this.tabAn[0].visibility = View.INVISIBLE
+            this.tabAn[2].visibility = View.INVISIBLE
+        }
+        this.tabAn[3].visibility = View.VISIBLE
+    }
+
+    fun fillQuizz(clickedView: View) {
+
     }
 }
