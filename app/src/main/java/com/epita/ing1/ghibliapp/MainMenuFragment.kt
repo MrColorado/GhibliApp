9e983c5b00d89a99dcfb02a9acf3cdb9964bb9b5
MrColorado <theo.issarni@epita.fr>
2018-06-07 13:01:19 +0200

    character: the liaison with the menu is working

---
+++ app/src/main/java/com/epita/ing1/ghibliapp/MainMenuFragment.kt
@@ -0,0 +1,39 @@
+package com.epita.ing1.ghibliapp
+
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.main_menu.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class MainMenuFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.main_menu, container, false)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        character_button.setOnClickListener {
+            val fragmentTransaction = fragmentManager!!.beginTransaction()
+            fragmentTransaction.replace(R.id.main_container, CharacterListFragment()).addToBackStack("CharacterList")
+            fragmentTransaction.commit()
+        }
+    }
+
+}
